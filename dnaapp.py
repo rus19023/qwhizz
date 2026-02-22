029db7b2b2046d5f298ba603058c8d82bfd86987
BonnieSites.Solutions <info@bonnysites.com>
2026-02-22 15:41:04 -0500

    crete new app files for modular usage

--- dnaapp.py
+++ dnaapp.py
@@ -1,13 +1,1 @@
-import streamlit as st
-
-# Page configuration MUST be first
-st.set_page_config(
-    page_title="🧬 DNA Study Gamified",
-    page_icon="🧬🧬",
-    layout="wide",
-    initial_sidebar_state="expanded"
-)
-
-# Theme setup (package handles its own paths automatically)
-from theme_switcher import quick_theme_setup
-quick_theme_setup(default_theme='retro')
+import qwhizz
